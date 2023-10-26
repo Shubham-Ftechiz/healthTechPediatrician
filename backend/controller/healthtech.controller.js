2f54cecbdea45c4bc6ebfadc5ac2ce028e5477a5
Shubham-Ftechiz <shubham.pal@ftechiz.com>
2023-10-26 18:03:38 +0530

    Create register APIs for healthTech

--- backend/controller/healthtech.controller.js
+++ backend/controller/healthtech.controller.js
@@ -1,23 +1,39 @@
 const User = require("../model/User");
+const bcrypt = require("bcryptjs");
 
 exports.getPediatricianInfo = async (req, res) => {
-  const insertUser = {
-    name: "Shubham",
-    email: "spal5776@gmail.com",
-    password: "12345",
-    date: Date.now(),
-    role: "Admin",
-    token: "shshshsskskk125ss@3",
-  };
-
-  User.create(insertUser)
+  User.find()
     .then((data) => {
-      console.log("InsertionData:", data);
+      res.send(data);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+};
+
+exports.register = async (req, res) => {
+  const r = req.body;
+
+  // Encrypt Password
+  const salt = await bcrypt.genSalt(10);
+  const encryptPass = await bcrypt.hash(r.password, salt);
+
+  // Decrypt Password
+
+  const result = bcrypt.compareSync(r.password, encryptPass);
+  // If true password match if false not match
+
+  r.password = encryptPass;
+
+  User.create(r)
+    .then((data) => {
       res.send({
         message: "User Created",
       });
     })
     .catch((err) => {
-      res.send(err);
+      res.send({
+        message: err.message,
+      });
     });
 };
--- backend/routes/getPediatrician.js
+++ backend/routes/getPediatrician.js
@@ -2,7 +2,15 @@ const getPediatrician = (app) => {
   const healthTech = require("../controller/healthtech.controller");
 
   const router = require("express").Router();
+
+  // Get getPediatrician Route
   router.get("/getPediatricianInfo", healthTech.getPediatricianInfo);
+
+  // Register Route
+  router.post("/register", healthTech.register);
+
+  // Login Route
+  //router.get("/login", healthTech.login);
 
   app.use("/api", router);
 };
