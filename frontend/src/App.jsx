f75c28ccdaa6e7fec1de7c2ebfdb5e1d5e66f750
AkshayBishtFtechiz <akshay.bisht1@ftechiz.com>
2023-11-09 18:03:37 +0530

    Registration Page Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,13 @@ import React from "react";
 import Dashboard from "./components/dashboard/dashboard-health-overview";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/login";
+import Registeration from "./components/register";
 
 const App = () => (
   <Routes>
-    <Route>
-      <Route path="/" element={<Login />} />
-    </Route>
-    <Route>
-      <Route path="/dashboard" element={<Dashboard/>} />
-    </Route>
+    <Route path="/" element={<Login />} />
+    <Route path="/dashboard" element={<Dashboard />} />
+    <Route path="/register" element={<Registeration />} />
   </Routes>
 );
 
