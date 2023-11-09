3c5675787ed568d6ebcf69e736aaebb6c9803663
KajalKushwahaDeveloper <kajalkushwaha231096@gmail.com>
2023-11-09 12:02:30 +0530

    login ui

--- frontend/src/components/Login/index.jsx
+++ frontend/src/components/Login/index.jsx
@@ -1,60 +1,113 @@
 import React from "react";
+import ReactDOM from "react-dom";
+// import "antd/dist/antd.css";
+
+import { Form, Input, Button, Checkbox } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+
 import "./login.scss";
 
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const login = () => {
-    const [loggedIn, setLoggedIn] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
 
-    const [username, setUsername] = useState("");
-    const [passsword, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [passsword, setPassword] = useState("");
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleLoggedIn = () => {
-        navigate("/dashboard");
-        setLoggedIn(true);
-        setTimeout(() => {
-            setLoggedIn(false);
-        }, 4000)
-    }
+  const onFinish = values => {
+    console.log('Received values of form: ', values);
+  };
 
-    const handleUserName = (e) => {
-        setUsername(e.target.value)
-    }
+  const handleLoggedIn = () => {
+    navigate("/dashboard");
+    setLoggedIn(true);
+    setTimeout(() => {
+      setLoggedIn(false);
+    }, 4000);
+  };
 
-    const handlePassword = (e) => {
-        setPassword(e.target.value)
-    }
+  const handleUserName = (e) => {
+    setUsername(e.target.value);
+  };
 
-    return (
-        <div className="mainLogin">
-             {loggedIn === true && username === "admin" && passsword === "admin" &&( 
-                <div className="successLogin"> Login Successfully </div>
-            )}
-            <div className="subLogin">
-                <div className="loginName">
-                Let's Get Started
-                </div>
-                <div className="loginTxt">
-                    <div className="username">
-                        <input type="text"
-                            onChange={(e)=> handleUserName(e)}
-                        placeholder="Username"/>
-                    </div>
-                    <div className="password">
-                        <input type="password"
-                            onChange={(e)=> handlePassword(e)}
-                        placeholder="Password"/>
-                    </div>
-                </div>
-                <div className="mainLogin">
-                    <button id="loginBtn" onClick={handleLoggedIn}> Login </button>
-                </div>
-            </div>            
-        </div>
-    )
-}
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  return (
+    <div className="mainLogin">
+      {loggedIn === true && username === "admin" && passsword === "admin" && (
+        <div className="successLogin"> Login Successfully </div>
+      )}
+      <div className="subLogin">
+        <h1>Let's Get Started</h1>
+        <p>Sign in to continue to Doclinic.</p>
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Username!",
+            },
+          ]}
+        >
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Username"
+        
+          />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Password!",
+            },
+          ]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <a className="login-form-forgot" href="">
+            Forgot password
+          </a>
+        </Form.Item>
+
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            Log in
+          </Button>
+          Or <a href="">register now!</a>
+        </Form.Item>
+      </Form>
+      </div>
+    </div>
+  );
+};
 
 export default login;
